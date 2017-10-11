e4c3e897c1f7a871b1f56a61fe472d882b513750
Peter Trana <peter.trana@jayway.com>
2017-10-11 08:35:43 +0200

    .

--- resolvers/app/components/HelloWorld/BarComponent/BarComponent.js
+++ resolvers/app/components/HelloWorld/BarComponent/BarComponent.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import styles from './BarComponent.pcss';
 import footil from '../../../utils/footil';
+import TextField from '../../../elements/TextField';
 import { FOO } from '../../../../shared/constants/foostant';
 
 class BarComponent extends Component {
@@ -8,6 +9,7 @@ class BarComponent extends Component {
     return (
       <div className={ styles.root }>
         <h1>Hello { footil() } { FOO }</h1>
+        <TextField />
       </div>
     );
   }
---
+++ resolvers/app/elements/TextField.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function () {
+  return (
+    <div>
+      <label htmlFor="bar">Field</label>
+      <input id="bar" type="text" />
+    </div>
+  );
+}
